f80ab88086f23c07db0e03bc9321f6587da01836
cs <cs@record-it.pl>
2024-10-09 14:10:48 +0200

    Initial commit

---
+++ Lecture2/Controllers/BookController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lectrure2.Controllers;
+
+public class BookController : Controller
+{
+    private Dictionary<int, FormBoook> books = new()
+    {
+        { 1, new FormBoook() { Author = "Freeman", Title = "C#", Id = 1 } },
+        { 2, new FormBoook() { Author = "Bloch", Title = "Java", Id = 2 } },
+        { 3, new FormBoook() { Author = "Freeman", Title = "JavaScript", Id = 3 } },
+    };
+    // GET
+    public IActionResult Index()
+    {
+        return View(books);
+    }
+
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        return View("Add", books[id]);
+    }
+    
+    [HttpPost]
+    public IActionResult Add(FormBoook book)
+    {
+        if (ModelState.IsValid)
+        {
+            return View("ConfirmBook", book);
+        }
+        else
+        {
+            return View();
+        }
+    }
+    
+    [HttpPost]
+    public IActionResult Edit(FormBoook book)
+    {
+        if (ModelState.IsValid)
+        {
+            return View("ConfirmBook", book);
+        }
+        else
+        {
+            return View();
+        }
+    }
+
+    public IActionResult Delete(int id)
+    {
+        books.Remove(id);
+        return View("Index", books);
+    }
+}
---
+++ Lecture2/Models/FormBoook.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc;
+
+public class FormBoook
+{
+    [HiddenInput]
+    public int Id { get; set; }
+    [MaxLength(length: 50)]
+    [Required(ErrorMessage = "Musisz podać tytuł!")]
+    public string Title { get; set; }
+    [MaxLength(length: 20)]
+    [Required]
+    public string Author { get; set; }
+}
